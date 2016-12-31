4860fd4823cb235f4b60ced1c00d910a1f11548a
bd3550 <bertrand.deloffre@free.fr>
2016-12-31 16:55:21 +0100

    Premier essai maquette

--- src/Louvre/TicketBundle/Controller/DefaultController.php
+++ src/Louvre/TicketBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('LouvreTicketBundle:Default:index.html.twig');
+        return $this->render('LouvreTicketBundle:Ticket:index.html.twig');
     }
 }
