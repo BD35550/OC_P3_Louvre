c0dcd9963830335bb2d02952daebdf5bd8c30e30
bd3550 <bertrand.deloffre@free.fr>
2017-01-18 10:28:23 +0100

    install stripe depuis composer

--- src/Louvre/TicketBundle/Controller/TicketController.php
+++ src/Louvre/TicketBundle/Controller/TicketController.php
@@ -51,33 +51,15 @@ class TicketController extends Controller
 
             $request->getSession()->getFlashBag()->add('info','');
             $id = $ticket->getId();
-            return $this->redirectToRoute('louvre_ticket_order_tree', compact('id','order'));
+            return $this->redirectToRoute('louvre_ticket_order_tree');
         }
 
         return $this->render('LouvreTicketBundle:Ticket:order_two.html.twig', ['form' => $form->createView()]);
     }
 
-    public function order_treeCreateAction(Order $order, Request $request)
+    public function order_treeCreateAction(Order $order)
     {
-        /*if ($request->isMethod('POST')) {
 
-            $payment = $this->get('louvre_ticket.stripe_payment');
-
-            try {
-                $payment->sendPayment($order, $request);
-            } catch (Exception $e) {
-                $request->getSession()->getFlashBag()->add('error','La carte saisie n\'est pas valide');
-
-                $id = $order->getId();
-                return $this->redirectToRoute('louvre_ticket_buy', compact('id'));
-            }
-            
-            $request->getSession()->getFlashBag()->add('success', 'Paiement validé, vous allez recevoir d\'ici quelques secondes un mail de confirmation accompagné des billets');
-
-            $id = $order->getId();
-            return $this->redirectToRoute('louvre_ticket_confirm', compact('id'));
-        }*/
-        // Création du formumlaire de paiement + Récap commande
-        return $this->render('LouvreTicketBundle:Ticket:order_tree.html.twig', compact('order'));
+        return $this->render('LouvreTicketBundle:Ticket:order_tree.html.twig');
     }
 }
